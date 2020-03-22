c16ab33235b4a11b9302bf20c81ddf3ba29f11c8
Scott Bass <scottbass47@gmail.com>
2020-03-22 14:32:37 -0400

    XOR test passed

---
+++ Assets/Scripts/XOREvaluator.cs
@@ -0,0 +1,147 @@
+﻿using SharpNeat.Core;
+using SharpNeat.Phenomes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XOREvaluator : IPhenomeEvaluator<IBlackBox>
+{
+    private const double StopFitness = 10.0;
+    private ulong evalCount;
+    private bool stopConditionSatisfied;
+
+    public ulong EvaluationCount => evalCount;
+    public bool StopConditionSatisfied => stopConditionSatisfied;
+
+    public FitnessInfo Evaluate(IBlackBox phenome)
+    {
+        double fitness = 0;
+        double output;
+        double pass = 1.0;
+        ISignalArray inputArr = phenome.InputSignalArray;
+        ISignalArray outputArr = phenome.OutputSignalArray;
+
+        evalCount++;            
+
+        //----- Test 0,0
+        phenome.ResetState();
+
+        // Set the input values
+        inputArr[0] = 0.0;
+        inputArr[1] = 0.0;
+
+        // Activate the black box.
+        phenome.Activate();
+        if(!phenome.IsStateValid) 
+        {   // Any black box that gets itself into an invalid state is unlikely to be
+            // any good, so lets just bail out here.
+            return FitnessInfo.Zero;
+        }
+
+        // Read output signal.
+        output = outputArr[0];
+        Debug.Assert(output >= 0.0, "Unexpected negative output.");
+
+        // Calculate this test case's contribution to the overall fitness score.
+        //fitness += 1.0 - output; // Use this line to punish absolute error instead of squared error.
+        fitness += 1.0-(output*output);
+        if(output > 0.5) {
+            pass = 0.0;
+        }
+
+    //----- Test 1,1
+        // Reset any black box state from the previous test case.
+        phenome.ResetState();
+
+        // Set the input values
+        inputArr[0] = 1.0;
+        inputArr[1] = 1.0;
+
+        // Activate the black box.
+        phenome.Activate();
+        if(!phenome.IsStateValid) 
+        {   // Any black box that gets itself into an invalid state is unlikely to be
+            // any good, so lets just bail out here.
+            return FitnessInfo.Zero;
+        }
+
+        // Read output signal.
+        output = outputArr[0];
+        Debug.Assert(output >= 0.0, "Unexpected negative output.");
+
+        // Calculate this test case's contribution to the overall fitness score.
+        //fitness += 1.0 - output; // Use this line to punish absolute error instead of squared error.
+        fitness += 1.0-(output*output);
+        if(output > 0.5) {
+            pass = 0.0;
+        }
+
+    //----- Test 0,1
+        // Reset any black box state from the previous test case.
+        phenome.ResetState();
+
+        // Set the input values
+        inputArr[0] = 0.0;
+        inputArr[1] = 1.0;
+
+        // Activate the black box.
+        phenome.Activate();
+        if(!phenome.IsStateValid) 
+        {   // Any black box that gets itself into an invalid state is unlikely to be
+            // any good, so lets just bail out here.
+            return FitnessInfo.Zero;
+        }
+
+        // Read output signal.
+        output = outputArr[0];
+        Debug.Assert(output >= 0.0, "Unexpected negative output.");
+
+        // Calculate this test case's contribution to the overall fitness score.
+        // fitness += output; // Use this line to punish absolute error instead of squared error.
+        fitness += 1.0-((1.0-output)*(1.0-output));
+        if(output <= 0.5) {
+            pass = 0.0;
+        }
+
+    //----- Test 1,0
+        // Reset any black box state from the previous test case.
+        phenome.ResetState();
+
+        // Set the input values
+        inputArr[0] = 1.0;
+        inputArr[1] = 0.0;
+
+        // Activate the black box.
+        phenome.Activate();
+        if(!phenome.IsStateValid) 
+        {   // Any black box that gets itself into an invalid state is unlikely to be
+            // any good, so lets just bail out here.
+            return FitnessInfo.Zero;
+        }
+
+        // Read output signal.
+        output = outputArr[0];
+        Debug.Assert(output >= 0.0, "Unexpected negative output.");
+
+        // Calculate this test case's contribution to the overall fitness score.
+        // fitness += output; // Use this line to punish absolute error instead of squared error.
+        fitness += 1.0-((1.0-output)*(1.0-output));
+        if(output <= 0.5) {
+            pass = 0.0;
+        }
+
+        // If all four outputs were correct, that is, all four were on the correct side of the
+        // threshold level - then we add 10 to the fitness.
+        fitness += pass * 10.0;
+
+        if(fitness >= StopFitness) {
+            stopConditionSatisfied = true;
+        }
+
+        return new FitnessInfo(fitness, fitness);
+    }
+
+    public void Reset()
+    {
+    }
+}
---
+++ Assets/Scripts/XORExperiment.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SharpNeat.Core;
+using SharpNeat.Network;
+using SharpNeat.Genomes.Neat;
+using SharpNeat.Decoders;
+using SharpNeat.EvolutionAlgorithms;
+using SharpNeat.Decoders.Neat;
+using SharpNeat.EvolutionAlgorithms.ComplexityRegulation;
+using SharpNeat.DistanceMetrics;
+using SharpNeat.SpeciationStrategies;
+using SharpNeat.Phenomes;
+
+public class XORExperiment : MonoBehaviour
+{
+    private void Start()
+    {
+        int populationSize = 50;
+        NetworkActivationScheme activationScheme = NetworkActivationScheme.CreateAcyclicScheme();
+
+        NeatGenomeParameters neatParams = new NeatGenomeParameters();
+        neatParams.ActivationFn = ReLU.__DefaultInstance;
+        neatParams.FeedforwardOnly = activationScheme.AcyclicNetwork;
+
+        IGenomeDecoder<NeatGenome, IBlackBox> neatDecoder = new NeatGenomeDecoder(activationScheme);
+
+        IGenomeFactory<NeatGenome> neatFactory = new NeatGenomeFactory(2, 1, neatParams);
+        List<NeatGenome> genomeList = neatFactory.CreateGenomeList(populationSize, 0);
+        XOREvaluator evaluator = new XOREvaluator();
+
+        ParallelOptions parallelOptions = new ParallelOptions();
+        IDistanceMetric distanceMetric = new ManhattanDistanceMetric(1.0, 0.0, 10.0);
+
+        // Evolution parameters
+        NeatEvolutionAlgorithmParameters neatEvolutionParams = new NeatEvolutionAlgorithmParameters();
+        neatEvolutionParams.SpecieCount = 10;
+        ISpeciationStrategy<NeatGenome> speciationStrategy = new ParallelKMeansClusteringStrategy<NeatGenome>(distanceMetric, parallelOptions);
+        IComplexityRegulationStrategy complexityRegulationStrategy = new DefaultComplexityRegulationStrategy(ComplexityCeilingType.Absolute, 50);
+
+        NeatEvolutionAlgorithm<NeatGenome> ea = new NeatEvolutionAlgorithm<NeatGenome>(neatEvolutionParams, speciationStrategy, complexityRegulationStrategy);
+        ea.UpdateScheme = new UpdateScheme(1);
+
+        IGenomeListEvaluator<NeatGenome> innerEvaluator = new ParallelGenomeListEvaluator<NeatGenome, IBlackBox>(neatDecoder, evaluator, parallelOptions);
+        IGenomeListEvaluator<NeatGenome> selectiveEvaluator = new SelectiveGenomeListEvaluator<NeatGenome>
+            (innerEvaluator, SelectiveGenomeListEvaluator<NeatGenome>.CreatePredicate_OnceOnly());
+
+        ea.Initialize(selectiveEvaluator, neatFactory, genomeList);
+        ea.UpdateEvent += (sender, e) =>
+        {
+            //Debug.Log("UPDATE");
+            //Debug.Log(ea.CurrentGeneration);
+        };
+        ea.PausedEvent += (sender, e) =>
+        {
+            //Debug.Log("PAUSED");
+            Debug.Log($"Finished on generation {ea.CurrentGeneration}");
+        };
+        ea.StartContinue();
+    }
+}
