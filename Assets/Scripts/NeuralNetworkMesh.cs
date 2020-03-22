14aa33f2cf1d49661d5fcd9f06f1019519d992cb
Scott Bass <scottbass47@gmail.com>
2020-03-22 18:47:18 -0400

    Switched to 3D

---
+++ Assets/Scripts/NeuralNetworkMesh.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NeuralNetworkMesh : Graphic
+{
+    private void Start()
+    {
+        Mesh mesh = new Mesh();
+        var canvasRenderer = GetComponent<CanvasRenderer>();
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+    }
+}
